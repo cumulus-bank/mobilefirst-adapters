aa6cc67f178668d6b22bf4b77ca0b0c0ba812016
saif <s4saif.121@gmail.com>
2019-11-24 13:05:57 +0400

    target generated

---
+++ banikingsvc/target/adapter/banikingsvc-impl.js
@@ -0,0 +1,232 @@
+/*
+ *  Licensed Materials - Property of IBM
+ *  5725-I43 (C) Copyright IBM Corp. 2011, 2016. All Rights Reserved.
+ *  US Government Users Restricted Rights - Use, duplication or
+ *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
+/**
+ * @param tag: a topic such as MobileFirst_Platform, Bluemix, Cordova.
+ * @returns json list of items.
+ */
+
+function getFeed(tag) {
+	var input = {
+	    method : 'get',
+	    returnedContentType : 'xml',
+	    path : getPath(tag)
+	};
+
+	return MFP.Server.invokeHttp(input);
+}
+
+/**
+ * Helper function to build the URL path.
+ */
+function getPath(tag){
+    if(tag === undefined || tag === ''){
+        return 'feed.xml';
+    } else {
+        return 'blog/atom/' + tag + '.xml';
+    }
+}
+
+/**
+ * @returns ok
+ */
+function unprotected(param) {
+	return {result : "Hello from unprotected resource"};
+}
+
+
+/**
+ * @returns 
+ * @param tag:
+ */
+function addNewAccount(tag){
+	var input = {
+		    method : 'post',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+			body: {
+				contentType: 'application/json' ,
+				content: tag
+			},
+		    path : '/listAccount'
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+/**
+ * @returns 
+ */
+function getAccount(){
+	var input = {
+		    method : 'get',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+		    path : '/listAccount'
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+
+/**
+ * @returns 
+ * @param tag 
+ */
+function getAccountById(tag){
+	var input = {
+		    method : 'get',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+		    path : '/listAccount/'+ tag
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+/**
+ * @returns 
+ * @param tag 
+ */
+function getAccountByID(tag){
+	var input = {
+		    method : 'get',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+		    path : '/listAccountID/'+ tag
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+
+/**
+ * @returns 
+ * @param tag 
+ */
+function updateAccount(tag){
+	var input = {
+		    method : 'put',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+		    path : '/listAccount/'+ tag
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+
+/**
+ * @returns 
+ * @param tag 
+ */
+function deleteAccount(tag){
+	var input = {
+		    method : 'delete',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+		    path : '/listAccount/'+ tag
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+/**
+ * @returns 
+ * @param tag:
+ */
+function activate(tag){
+	var input = {
+		    method : 'post',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+			body: {
+				contentType: 'application/json' ,
+				content: tag
+			},
+		    path : '/activate'
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+/**
+ * @returns 
+ * @param tag:
+ */
+function bill(tag){
+	var input = {
+		    method : 'post',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+			body: {
+				contentType: 'application/json' ,
+				content: tag
+			},
+		    path : '/bill'
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+/**
+ * @returns 
+ * @param tag:
+ */
+function balanceUpdate(tag){
+	var input = {
+		    method : 'post',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+			body: {
+				contentType: 'application/json' ,
+				content: tag
+			},
+		    path : '/balanceUpdate'
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
+
+/**
+ * @returns 
+ * @param tag:
+ */
+function transaction(tag){
+	var input = {
+		    method : 'post',
+			returnedContentType : 'json',
+			headers: {
+				"Content-Type" : "application/json",
+			},
+			body: {
+				contentType: 'application/json' ,
+				content: tag
+			},
+		    path : '/transaction'
+		};
+	var result =  MFP.Server.invokeHttp(input);
+	return result
+}
